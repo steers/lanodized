c92f1ee939263f04234e9a50b0e7af322949c224
Alexander Steers <alex.steers@gmail.com>
2017-09-17 20:37:58 -0400

    Add chat message handler

---
+++ src/events/message.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = (client, message) => {
+  // avoid getting caught in bot spam loops
+  if (message.author.bot) return;
+  // only respond to commands using the configured prefix
+  if (message.content.indexOf(client.config.prefix) !== 0) return;
+
+  const args = message.content.trim().split(/\s+/g);
+  const command = args.shift().slice(client.config.prefix.length).toLowerCase();
+
+  const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command));
+  if (cmd) {
+    message.flags = [];
+    while (args[0] && args[0].match(/^-+/)) {
+      message.flags.push(args.shift().replace(/^-+/, ''));
+    }
+    cmd.run(client, message, args);
+  }
+};
