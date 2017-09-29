a36cf46ecd083bd0e88fd657e81d907a57d57269
Alexander Steers <alex.steers@gmail.com>
2017-09-28 22:21:34 -0400

    Use async initializer for models

--- src/models/index.js
+++ src/models/index.js
@@ -1,36 +1,36 @@
 'use strict';
-
-const fs = require('fs');
-const path = require('path');
 const Sequelize = require('sequelize');
+const {promisify} = require('util');
+const readdir = promisify(require('fs').readdir);
+const path = require('path');
 const basename = path.basename(module.filename);
+
+const db = {};
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/database.json')[env];
-const db = {};
 
 const sequelize = (config.use_env_variable
   ? new Sequelize(process.env[config.use_env_variable])
   : new Sequelize(config.database, config.username, config.password, config));
 
-fs
-  .readdirSync(__dirname)
-  .filter((file) => {
+async function initialize() {
+  const files = await readdir(__dirname);
+  files.filter((file) => {
     return (file.indexOf('.') !== 0)
         && (file !== basename)
         && (file.slice(-3) === '.js');
-  })
-  .forEach((file) => {
-    let model = sequelize['import'](path.join(__dirname, file));
+  }).forEach((file) => {
+    let model = sequelize['import'](path.resolve(__dirname, file));
     db[model.name] = model;
   });
+  Object.keys(db).forEach((modelName) => {
+    if (db[modelName].associate) {
+      db[modelName].associate(db);
+    }
+  });
+  db.sequelize = sequelize;
+  db.Sequelize = Sequelize;
+  return db;
+}
 
-Object.keys(db).forEach((modelName) => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
+module.exports.initialize = initialize;
