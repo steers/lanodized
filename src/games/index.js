66dd0c068f606d2f64472b60e768e4e9b2f911c4
Alexander Steers <alex.steers@gmail.com>
2017-09-28 20:53:37 -0400

    Use fancy async functions for initialization

--- src/games/index.js
+++ src/games/index.js
@@ -1,5 +1,6 @@
 'use strict';
-const fs = require('fs');
+const {promisify} = require('util');
+const readdir = promisify(require('fs').readdir);
 const path = require('path');
 const basename = path.basename(module.filename);
 
@@ -8,14 +9,14 @@ const basename = path.basename(module.filename);
  * database, complete with aliases, game modes, genres
  * and platforms.
  *
- * @param  {Object} db Sequelize instance with models.
+ * @param  {Object} ctx Application context, including db instance.
  * @param  {Object} definition Complete game definition to sync.
  * @return {Promise} Transaction containing the entire upsert.
  */
-function upsertGame(db, definition) {
-  return db.sequelize.transaction((t) => {
+function upsertGame(ctx, definition) {
+  return ctx.db.sequelize.transaction((t) => {
     let game;
-    return db.Game.findOne({
+    return ctx.db.Game.findOne({
       transaction: t,
       where: {
         title: definition.info.title,
@@ -26,14 +27,14 @@ function upsertGame(db, definition) {
           transaction: t,
         });
       } else {
-        return db.Game.create(definition.info, {
+        return ctx.db.Game.create(definition.info, {
           transaction: t,
         });
       }
     }).then((entity) => {
       game = entity;
       return Promise.all([
-        db.Genre.findAll({
+        ctx.db.Genre.findAll({
           transaction: t,
           where: {
             short: {
@@ -41,7 +42,7 @@ function upsertGame(db, definition) {
             },
           },
         }),
-        db.Platform.findAll({
+        ctx.db.Platform.findAll({
           transaction: t,
           where: {
             short: {
@@ -56,7 +57,7 @@ function upsertGame(db, definition) {
       return game.save({transaction: t});
     }).then((entity) => {
       const aliases = definition.aliases.map((alias) => {
-        return db.GameAlias.upsert({
+        return ctx.db.GameAlias.upsert({
           name: alias,
           GameId: game.id,
         }, {
@@ -66,7 +67,7 @@ function upsertGame(db, definition) {
       return Promise.all(aliases);
     }).then((created) => {
       const modes = definition.modes.map((mode) => {
-        return db.GameMode.upsert(Object.assign({
+        return ctx.db.GameMode.upsert(Object.assign({
           GameId: game.id,
         }, mode), {
           transaction: t,
@@ -74,26 +75,25 @@ function upsertGame(db, definition) {
       });
       return Promise.all(modes);
     }).then((created) => {
-      console.log(`Upsert complete for: ${game.name}`);
+      ctx.log(`Upsert complete for: ${game.title}`);
     });
   });
 }
 
-module.exports = {
-  initialize: (db) => {
-    const ops = [];
-    fs
-      .readdirSync(__dirname)
-      .filter((file) => {
-        return (file.indexOf('.') !== 0)
-            && (file !== basename)
-            && (file.slice(-3) === '.js');
-      })
-      .forEach((file) => {
-        let game = require(path.resolve(__dirname, file));
-        ops.push(upsertGame(db, game));
-        delete require.cache[require.resolve(path.resolve(__dirname, file))];
-      });
-    return Promise.all(ops);
-  },
-};
+async function initialize(ctx, client) {
+  const ops = [];
+  const games = await readdir(__dirname);
+  games.filter((file) => {
+    return (file.indexOf('.') !== 0)
+        && (file !== basename)
+        && (file.slice(-3) === '.js');
+  }).forEach((file) => {
+    let fullpath = path.resolve(__dirname, file);
+    let game = require(fullpath);
+    ops.push(upsertGame(ctx, game));
+    delete require.cache[require.resolve(fullpath)];
+  });
+  await Promise.all(ops);
+}
+
+module.exports.initialize = initialize;
