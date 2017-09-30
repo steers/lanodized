33ddb84043e4968ff84308b46a9d29e7ff8adf31
Alexander Steers <alex.steers@gmail.com>
2017-09-29 23:28:39 -0400

    Message handler gets to use the shiny new parser library

--- src/events/message.js
+++ src/events/message.js
@@ -1,22 +1,19 @@
 'use strict';
 
+const {tokenize} = require('../lib/parser');
+
 module.exports = (ctx, client, message) => {
   // avoid getting caught in bot spam loops
   if (message.author.bot) return;
   // only respond to commands using the configured prefix
   if (message.content.indexOf(client.config.prefix) !== 0) return;
 
-  const args = message.content.trim().split(/\s+/g);
-  const command = args.shift().slice(client.config.prefix.length).toLowerCase();
-
+  const args = tokenize(message.content.slice(client.config.prefix.length));
+  const command = args.shift().toLowerCase();
   const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command));
-  if (cmd) {
-    message.flags = [];
-    while (args[0] && args[0].match(/^-+/)) {
-      message.flags.push(args.shift().replace(/^-+/, ''));
-    }
-    cmd.run(client, message, args).then(() => {
-      // TODO: Measure command usage
-    });
-  }
+
+  if (!cmd) return;
+  cmd.run(ctx, client, args).then(() => {
+    // TODO: Measure command usage
+  });
 };
