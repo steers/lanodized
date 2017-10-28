50e09137c684fa0ffe92ab32b39035c2ac0b0902
Alexander Steers <alex.steers@gmail.com>
2017-10-28 11:23:31 -0400

    Fix sequelize warning about operator aliases

--- src/models/index.js
+++ src/models/index.js
@@ -9,6 +9,43 @@ const db = {};
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config').database[env];
 
+config.operatorsAliases = {
+  $eq: Sequelize.Op.eq,
+  $ne: Sequelize.Op.ne,
+  $gte: Sequelize.Op.gte,
+  $gt: Sequelize.Op.gt,
+  $lte: Sequelize.Op.lte,
+  $lt: Sequelize.Op.lt,
+  $not: Sequelize.Op.not,
+  $in: Sequelize.Op.in,
+  $notIn: Sequelize.Op.notIn,
+  $is: Sequelize.Op.is,
+  $like: Sequelize.Op.like,
+  $notLike: Sequelize.Op.notLike,
+  $iLike: Sequelize.Op.iLike,
+  $notILike: Sequelize.Op.notILike,
+  $regexp: Sequelize.Op.regexp,
+  $notRegexp: Sequelize.Op.notRegexp,
+  $iRegexp: Sequelize.Op.iRegexp,
+  $notIRegexp: Sequelize.Op.notIRegexp,
+  $between: Sequelize.Op.between,
+  $notBetween: Sequelize.Op.notBetween,
+  $overlap: Sequelize.Op.overlap,
+  $contains: Sequelize.Op.contains,
+  $contained: Sequelize.Op.contained,
+  $adjacent: Sequelize.Op.adjacent,
+  $strictLeft: Sequelize.Op.strictLeft,
+  $strictRight: Sequelize.Op.strictRight,
+  $noExtendRight: Sequelize.Op.noExtendRight,
+  $noExtendLeft: Sequelize.Op.noExtendLeft,
+  $and: Sequelize.Op.and,
+  $or: Sequelize.Op.or,
+  $any: Sequelize.Op.any,
+  $all: Sequelize.Op.all,
+  $values: Sequelize.Op.values,
+  $col: Sequelize.Op.col,
+};
+
 const sequelize = (config.use_env_variable
   ? new Sequelize(process.env[config.use_env_variable])
   : new Sequelize(config.database, config.username, config.password, config));
