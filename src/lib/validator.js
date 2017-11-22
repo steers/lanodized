eb49a3259b53ed2aae7a7f2bef13e984867728de
Alexander Steers <alex.steers@gmail.com>
2017-11-22 09:46:20 -0500

    Update data file validator
    Some of the custom validators just flat out did not work.
    
    Also, added a spec for party definitions (processing TBD)

--- src/lib/validator.js
+++ src/lib/validator.js
@@ -4,6 +4,22 @@ const {arrayify} = require('./parser');
 const moment = require('moment-timezone');
 
 /**
+ * Representation of a validation failure.
+ */
+class ValidationError extends Error {
+  /**
+   * Create a new ValidationError
+   * @param  {string} message Error description
+   * @param  {Object} reasons Validation failure reasons for each invalid parameter
+   */
+  constructor(message, reasons = {}) {
+    super(message);
+    this.name = 'ValidationError';
+    this.reasons = reasons;
+  }
+}
+
+/**
  * Validate the structure of data files (typically stored as JSON)
  */
 class DataFileValidator {
@@ -28,21 +44,40 @@ class DataFileValidator {
       }
     };
     this.validate.validators.array = (value, options) => {
-      if (!Array.isArray(value)) {
+      if (!this.validate.isArray(value)) {
         return 'must be an array';
       }
 
-      value.forEach((item) => {
-        this.validate(item, options);
-      });
+      const failures = [];
+      for (const item of value) {
+        const invalid = this.validate.isObject(item)
+          ? this.validate(item, options)
+          : this.validate.single(item, options);
+        if (invalid) {
+          failures.push(...arrayify(invalid));
+        }
+      };
+      if (failures.length > 0) {
+        return failures;
+      }
     };
     this.validate.validators.eachProperty = (value, options) => {
+      if (!this.validate.isEmpty()) return;
       if (!this.validate.isObject(value) || this.validate.isArray(value)) {
         return 'must be an object';
       }
 
-      for (const prop of Object.keys(value)) {
-        this.validate(value[prop], options);
+      const failures = [];
+      for (const property of Object.keys(value)) {
+        const invalid = this.validate.isObject(value[property])
+          ? this.validate(value[property], options)
+          : this.validate.single(value[property], options);
+        if (invalid) {
+          failures.push(...arrayify(invalid));
+        }
+      }
+      if (failures.length > 0) {
+        return failures;
       }
     };
     this.validate.extend(this.validate.validators.datetime, {
@@ -64,9 +99,17 @@ class DataFileValidator {
    * @param  {Object} ctx.db Database context
    */
   async load(ctx) {
-    this.genres = await ctx.db.Genre.findAll();
-    this.platforms = await ctx.db.Platform.findAll();
-    this.eventTypes = await ctx.db.EventType.findAll();
+    this.genres = await ctx.db.Genre.findAll({
+      attributes: ['short'],
+    }).map((genre) => genre.short);
+
+    this.platforms = await ctx.db.Platform.findAll({
+      attributes: ['short'],
+    }).map((platform) => platform.short);
+
+    this.eventTypes = await ctx.db.EventType.findAll({
+      attributes: ['name'],
+    }).map((type) => type.name);
   }
 
   /**
@@ -75,79 +118,169 @@ class DataFileValidator {
    */
   validateGame(definition) {
     const self = this;
-    const mode = {
-      'name': {
-        format: self.constructor.TITLE_PATTERN,
-      },
-      'short': {
-        presence: true,
-        format: self.constructor.SHORT_PATTERN,
-      },
-      'serverSize': {
-        presence: true,
-        numericality: {
-          strict: true,
-          onlyInteger: true,
-          greaterThan: 1,
-        },
-      },
-      'groupSize': {
-        presence: true,
-        numericality: {
-          strict: true,
-          onlyInteger: true,
-          greaterThan: 1,
-        },
-      },
-      'description': {},
-      'properties': {},
-    };
     const constraints = {
-
-      'aliases': {
+      'info': {
         presence: true,
-        length: {minimum: 1},
-        array: {
-          format: self.constructor.SHORT_PATTERN,
-        },
       },
       'info.title': {
         presence: true,
-        format: self.constructor.TITLE_PATTERN,
+        format: {
+          pattern: self.constructor.TITLE_PATTERN,
+          message: `'%{value}' is invalid`,
+        },
       },
       'info.released': {
         date: true,
       },
-      'info.links': {
-        containedBy: self.platforms.map((platform) => platform.short),
-        eachProperty: {
-          'info': {
-            url: true,
-          },
-          'buy': {
-            url: true,
+      'info.links.buy': {
+        eachProperty: {url: true},
+        containedBy: ['default'].concat(self.platforms),
+      },
+      'info.links.info': {
+        eachProperty: {url: true},
+        containedBy: ['default'].concat(self.platforms),
+      },
+      'info.properties.free': {
+        inclusion: [true, false],
+      },
+      'info.properties.steamId': {
+        numericality: {
+          strict: true,
+          onlyInteger: true,
+          greaterThan: 0,
+        },
+      },
+      'aliases': {
+        presence: true,
+        length: {minimum: 1},
+        array: {
+          format: {
+            pattern: self.constructor.SHORT_PATTERN,
+            message: `'%{value}' is invalid`,
           },
         },
       },
       'genres': {
         presence: true,
         length: {minimum: 1},
-        containedBy: self.genres.map((genre) => genre.short),
+        containedBy: self.genres,
       },
       'platforms': {
         presence: true,
         length: {minimum: 1},
-        containedBy: self.platforms.map((platform) => platform.short),
+        containedBy: self.platforms,
       },
       'modes': {
         presence: true,
         length: {minimum: 1},
-        array: mode,
+        array: {
+          'name': {
+            format: {
+              pattern: self.constructor.TITLE_PATTERN,
+              message: `'%{value}' is invalid`,
+            },
+          },
+          'short': {
+            presence: true,
+            format: {
+              pattern: self.constructor.SHORT_PATTERN,
+              message: `'%{value}' is invalid`,
+            },
+          },
+          'serverSize': {
+            presence: true,
+            numericality: {
+              strict: true,
+              onlyInteger: true,
+              greaterThan: 1,
+            },
+          },
+          'groupSize': {
+            presence: true,
+            numericality: {
+              strict: true,
+              onlyInteger: true,
+              greaterThan: 0,
+            },
+          },
+          'description': {},
+          'properties': {},
+        },
       },
     };
-    const invalidDefinition = this.validate(definition, constraints, {format: 'flat'});
+    const invalidDefinition = this.validate(definition, constraints, {fullMessages: false});
     if (invalidDefinition) {
-      throw new Error(`Invalid game: ${JSON.stringify(invalidDefinition)}`);
+      throw new ValidationError('Game definition does not meet specifications.', invalidDefinition);
+    }
+  }
+
+  /**
+   * Ensure the given party definition meets specifications. Will throw if invalid.
+   * @param  {Object} definition Party definition
+   */
+  validateParty(definition) {
+    const self = this;
+    const constraints = {
+      'name': {
+        presence: true,
+        format: self.constructor.TITLE_PATTERN,
+      },
+      'iteration': {
+        presence: true,
+        numericality: {
+          strict: true,
+          onlyInteger: true,
+          greaterThanOrEqualTo: 1,
+        },
+      },
+      'start': {
+        presence: true,
+        datetime: true,
+      },
+      'end': {
+        presence: true,
+        datetime: true,
+      },
+      'events': {
+        array: {
+          'type': {
+            presence: true,
+            containedBy: self.eventTypes,
+          },
+          'title': {
+            presence: {allowEmpty: false},
+            length: {maximum: 255},
+          },
+          'start': {
+            presence: true,
+            datetime: true,
+          },
+          'minutes': {
+            presence: true,
+            numericality: {
+              strict: true,
+              greaterThan: 0,
+            },
+          },
+          'games': {
+            array: {
+              format: {
+                pattern: self.constructor.SHORT_PATTERN,
+                message: `'%{value}' is invalid`,
+              },
+            },
+          },
+          'description': {},
+          'properties': {},
+          'properties.sponsor': {
+            format: self.constructor.TITLE_PATTERN,
+          },
+        },
+      },
+    };
+    const invalidDefinition = this.validate(definition, constraints, {fullMessages: false});
+    if (invalidDefinition) {
+      throw new ValidationError('Party definition does not meet specifications.', invalidDefinition);
     }
   }
 }
@@ -156,8 +289,9 @@ class DataFileValidator {
 DataFileValidator.SHORT_PATTERN = /[-_a-z0-9]+/i;
 
 // Ensure titles are sanitized
-DataFileValidator.TITLE_PATTERN = /[-_:' a-z0-9]+/i;
+DataFileValidator.TITLE_PATTERN = /[-_:'.,!?() a-z0-9]+/i;
 
 module.exports = {
   DataFileValidator,
+  ValidationError,
 };
